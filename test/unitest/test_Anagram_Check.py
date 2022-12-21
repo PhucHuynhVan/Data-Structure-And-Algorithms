7d2c3925ca182d06a756fee3cb68e101754640bf
PhucHuynhVan <phuc.huynhvanspkt@gmail.com>
2022-12-21 22:23:37 +0700

    test yml

--- test/unitest/test_Anagram_Check.py
+++ test/unitest/test_Anagram_Check.py
@@ -7,7 +7,7 @@ from Array_Sequences.Anagram_Check.Anagram_Check import anagram_check
     ('abc', 'cba', True),
     ('hi man', 'hi     man', True),
     ('aabbcc', 'aabbc', False),
-    ('123', '1 2', False)
+    ('123', '1 2', True)
 ])
 def test_anagram_func(str_1, str_2, out):
     assert anagram_check(str_1, str_2) == out
