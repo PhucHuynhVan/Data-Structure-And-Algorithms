fa623c5799151cf9a439673dbd5758cdf0c6ec87
Phuc Huynh Van <119332757+PhucHuynhVan@users.noreply.github.com>
2022-12-23 12:37:13 +0700

    Create largest_continuous_sum.py

---
+++ largest_continuous_sum/largest_continuous_sum.py
@@ -0,0 +1,15 @@
+"""Largest continuous sum"""
+from typing import List
+
+
+def largest_continuous_sum(arr: List[int]) -> int:
+    "largest_continuous_sum func"
+    if len(arr) == 0:
+        return 0
+
+    current_sum = max_sum = arr[0]
+    for num in arr[1:]:
+        current_sum = max(current_sum+num, num)
+
+        max_sum = max(current_sum, max_sum)
+    return max_sum
