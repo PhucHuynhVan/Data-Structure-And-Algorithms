367d5171a69cf1255c89a2df2b5f9b1ceacd0048
PhucHuynhVan <phuc.huynhvanspkt@gmail.com>
2023-01-04 23:05:05 +0700

    Recursion Homework Problems: returns the sum of all the individual digits in that integer

--- recursion/recursion_homework_problems.py
+++ recursion/recursion_homework_problems.py
@@ -7,3 +7,11 @@ def recursion_calculate_sum_n_integer(numbers: int) -> int:
         return 0
 
     return numbers + recursion_calculate_sum_n_integer(numbers-1)
+
+
+def recursion_sum_of_all_the_individual_digits(numbers: int) -> int:
+    """returns the sum of all the individual digits in that integer. """
+    if numbers < 10:
+        return numbers
+
+    return numbers % 10 + recursion_sum_of_all_the_individual_digits(int(numbers/10))
--- test/unitest/test_recursion_homework_problems.py
+++ test/unitest/test_recursion_homework_problems.py
@@ -1,6 +1,7 @@
 import pytest
 from recursion.recursion_homework_problems import (
     recursion_calculate_sum_n_integer,
+    recursion_sum_of_all_the_individual_digits,
 )
 
 
@@ -10,3 +11,11 @@ from recursion.recursion_homework_problems import (
 ])
 def test_recursion_calculate_sum_n_integer(number, out):
     assert recursion_calculate_sum_n_integer(number) == out
+
+
+@pytest.mark.parametrize("number, out", [
+    (4231, 10),
+    (9, 9),
+])
+def test_recursion_sum_of_all_the_individual_digits(number, out):
+    assert recursion_sum_of_all_the_individual_digits(number) == out
