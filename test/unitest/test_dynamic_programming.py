9d79c190abe337abcd117d20fc826f8e972413f0
Phuc Huynh Van <119332757+PhucHuynhVan@users.noreply.github.com>
2023-01-16 16:11:48 +0700

    Add testcases for Grid Traveler

--- test/unitest/test_dynamic_programming.py
+++ test/unitest/test_dynamic_programming.py
@@ -1,5 +1,8 @@
 import pytest
-from dynamic_programming.dynamic_programming import fib_memoization
+from dynamic_programming.dynamic_programming import (
+    fib_memoization,
+    grid_traveler,
+)
 
 
 @pytest.mark.parametrize("n, out", [
@@ -8,3 +11,11 @@ from dynamic_programming.dynamic_programming import fib_memoization
 ])
 def test_fib_memoization_func(n, out):
     assert fib_memoization(n) == out
+
+
+@pytest.mark.parametrize("m, n, out", [
+    (2, 3, 3),
+    (18, 18, 2333606220)
+])
+def test_grid_traveler_func(m, n, out):
+    assert grid_traveler(m, n, {}) == out
