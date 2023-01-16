18f4e7a127ca1a1d3c285e2a1e3b76534f520d8d
Phuc Huynh Van <119332757+PhucHuynhVan@users.noreply.github.com>
2023-01-16 16:11:10 +0700

    Grid traveler source

--- dynamic_programming/dynamic_programming.py
+++ dynamic_programming/dynamic_programming.py
@@ -5,5 +5,22 @@ def fib_memoization(number: int) -> int:
     """Fibonnaci uses memoization"""
     memo = [0, 1]
     for i in range(2, number + 1):
-        memo.append(memo[i-1] + memo[i-2])
+        memo.append(memo[i - 1] + memo[i - 2])
     return memo[number]
+
+
+def grid_traveler(rows: int, columns: int, memoization: {}) -> int:
+    """
+    Return the number of paths from the top-left to the bottom-right of the grids (rows x columns)
+    """
+    matrix_key = str(rows) + "," + str(columns)
+    if matrix_key in memoization:
+        return memoization[matrix_key]
+    if (rows == 0) or (columns == 0):
+        return 0
+    if (rows == 1) and (columns == 1):
+        return 1
+    memoization[matrix_key] = grid_traveler(
+        rows - 1, columns, memoization
+    ) + grid_traveler(rows, columns - 1, memoization)
+    return memoization[matrix_key]
